8d9b52be11d36d28761a8a8da65f62952c795d88
Sammy92dec Tekheste <samrawit.samuel.tk@gmail.com>
2024-05-18 08:35:19 +0000

    Deploy to heroku

--- main/settings.py
+++ main/settings.py
@@ -24,12 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-=vvbf=6i4ffm4qu9rr0#f79(w6kx(n*6g#kd##@l^wo6$s^1w*'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['8000-sammy92dec-gymsam-4wjv0l2rnsb.ws-eu111.gitpod.io']
+ALLOWED_HOSTS = ['8000-sammy92dec-gymsam-4wjv0l2rnsb.ws-eu111.gitpod.io','.herokuapp.com']
 
 
 # Application definition
